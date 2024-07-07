5040b39e9a2004bcc3e9458b65d6aca330d42b17
hazxel <steven.b.zhou@foxmail.com>
2024-07-08 00:46:43 +0800

    let okx ws client manage ws connection

--- src/python/record_trade_data_ws.py
+++ src/python/record_trade_data_ws.py
@@ -2,7 +2,6 @@ import asyncio
 import csv
 import sys
 import time
-import websockets
 
 from dumbtrader.api.okxws.client import *
 from dumbtrader.api.okxws.constants import *
@@ -11,10 +10,7 @@ from dumbtrader.api.okxws.constants import *
 async def ws_channel_dump_csv(inst_id, new_file_period_s, dump_path, col_filter=[]):
     while True:
         try:
-            async with websockets.connect(OKX_WS_URI.PUBLIC_BUSINESS) as ws:
-                client = OkxWsClient(ws)
-                response = await client.subscribe(OKX_WS_SUBSCRIBE_CHANNEL.TRADES_ALL, inst_id)
-                print((f"subscribe success, connection id: {response}"))
+            async with OkxWsListenTradesAllClient(OKX_WS_URI.PUBLIC_BUSINESS, inst_id) as client:
                 while True:
                     period_ts_prefix = int(time.time()) // new_file_period_s
                     filename = f"{dump_path}/{inst_id}-{OKX_WS_SUBSCRIBE_CHANNEL.TRADES_ALL}-{period_ts_prefix}.csv"
@@ -31,7 +27,7 @@ async def ws_channel_dump_csv(inst_id, new_file_period_s, dump_path, col_filter=
                             row = await client.recv_data(col_filter)
                             writer.writerow(row)
         except Exception as e:
-            print(f"Caught a general exception: {e}, setting up new websockt connection...")
+            print(f"Caught a general exception: {e}, setting up new okx websocket client...")
 
 
 async def ws_channel_dump_tradeall_csv(inst_id_list, dump_path="."):
@@ -39,7 +35,7 @@ async def ws_channel_dump_tradeall_csv(inst_id_list, dump_path="."):
         asyncio.create_task(ws_channel_dump_csv(
             inst_id = inst_id,
             new_file_period_s = 10000,
-            path = dump_path,
+            dump_path = dump_path,
             col_filter=['instId', 'tradeId']
         )) 
         for inst_id in inst_id_list
