100ae29de378fcc42118534351583b5db24821d4
hazxel <steven.b.zhou@foxmail.com>
2024-07-21 11:04:23 +0800

    add multi ema strategy

---
+++ src/python/backtest_multi_ema.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import sys
+
+from dumbtrader.backtest.backtester import *
+from dumbtrader.strategy.multi_ema_strategy import *
+
+if __name__ == '__main__':
+    data_files = find_files_with_prefix(sys.argv[1], "ETH-USDT-SWAP-trades-all-172")
+    stra = MultiEmaStrategy("ETH-USDT", 1, 50000, 100000, 200000, 800000)
+
+    bt = backetester(stra, data_files)
+    pxs, pnls = bt.run()
+
+    print(bt.summary())
+    print(f"win: {stra.win}, loss: {stra.loss}") # win rate: {100*stra.win/(stra.win+stra.loss):.2f}%")
+
+    start = 40000
+    end = -1
+    step = 100
+
+    pnls = pnls[start:end:step]
+    pxs = pxs[start:end:step]
+    x_values = range(len(pnls))
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(x_values, pnls, marker='s', markersize=0.5, linewidth=0.3, linestyle='-', color='r', label='pnl')
+    ax1.set_ylabel('pnl', color='r')
+    ax1.tick_params(axis='y', labelcolor='r')
+
+    ax2 = ax1.twinx()
+    ax2.plot(x_values, pxs, marker='o', markersize=0.5, linewidth=0.3, linestyle=':', color='b', label='price')
+    ax2.set_ylabel('instrument price', color='b')
+    ax2.tick_params(axis='y', labelcolor='b')
+
+    ax2.plot(x_values, stra.emas[0][start:end:step], marker='o', markersize=0.2, linewidth=0.1, linestyle='-.', label='ema')
+    ax2.plot(x_values, stra.emas[1][start:end:step], marker='o', markersize=0.2, linewidth=0.1, linestyle='-.', label='ema')
+    ax2.plot(x_values, stra.emas[2][start:end:step], marker='o', markersize=0.2, linewidth=0.1, linestyle='-.', label='ema')
+    ax2.plot(x_values, stra.emas[3][start:end:step], marker='o', markersize=0.2, linewidth=0.1, linestyle='-.', label='ema')
+
+    
+    plt.title('Ema strategy')
+    plt.xlabel('time')
+    # plt.ylabel('pnl')
+    plt.show()
---
+++ src/python/dumbtrader/strategy/multi_ema_strategy.py
@@ -0,0 +1,112 @@
+from enum import Enum, auto
+import uuid
+
+from dumbtrader.strategy.strategy import *
+
+class Position(Enum):
+    LONG = auto()
+    SHORT = auto()
+    EMPTY = auto()
+
+
+class MultiEmaStrategy(Strategy):
+    def __init__(self, inst_id, weight, *periods):
+        self.inst_id = inst_id
+        self.weight = weight
+        self.periods = list(periods)
+        self.periods.sort() # fast ema first
+        self.alpha = [ 1 / period for period in self.periods]
+        self.alphadiff = [ 1 - alpha for alpha in self.alpha]
+        self.ema_count = len(self.periods)
+        self.emas = None
+        
+        self.init_ticks = 0
+        self.position = Position.EMPTY
+        self.continue_counts = 0
+        self.continue_side = Position.EMPTY
+
+        self.win = 0
+        self.loss = 0
+        self.open_px = None
+
+    def sell_signal(self):
+        id = str(uuid.uuid4()).replace('-', '')
+        return [(Signal.SUBMIT, MktSellOrder(self.inst_id, self.weight, id))]
+
+    def buy_signal(self):
+        id = str(uuid.uuid4()).replace('-', '')
+        return [(Signal.SUBMIT, MktBuyOrder(self.inst_id, self.weight, id))]
+
+    def on_start(self, px_tuple):
+        self.emas = [[px_tuple['px']] for _ in range(self.ema_count)]
+        return []
+    
+    def on_order_filled(self, internal_ord_id):
+        return []
+    
+    def on_px_change(self, px_tuple):
+        for i in range(self.ema_count):
+            self.emas[i].append(self.alphadiff[i] * self.emas[i][-1] + px_tuple['px'] * self.alpha[i])
+
+        if self.init_ticks < self.periods[-1]:
+            self.init_ticks += 1
+            return []
+        
+        isLong = px_tuple['px'] > self.emas[0][-1]
+        isShort = px_tuple['px'] < self.emas[0][-1]
+        for i in range(self.ema_count - 1):
+            if self.emas[i][-1] <= self.emas[i+1][-1]:
+                isLong = False
+            if self.emas[i][-1] >= self.emas[i+1][-1]:
+                isShort = False
+            if (not isLong) and (not isShort):
+                break
+        
+        if self.position == Position.EMPTY:
+            if isShort:
+                if self.continue_side == Position.SHORT:
+                    if self.continue_counts > self.periods[0]:
+                        self.position = Position.SHORT
+                        self.open_px = px_tuple['px']
+                        return self.sell_signal()
+                    else:
+                        self.continue_counts += 1
+                else:
+                    self.continue_counts = 0
+                    self.continue_side = Position.SHORT
+            elif isLong:
+                if self.continue_side == Position.LONG:
+                    # no! just self.emas[-2][-1] -5 > self.emas[-1][-1]
+                    if self.continue_counts > self.periods[0]:
+                        self.position = Position.LONG
+                        self.open_px = px_tuple['px']
+                        return self.buy_signal()
+                    else:
+                        self.continue_counts += 1
+                else:
+                    self.continue_counts = 0
+                    self.continue_side = Position.LONG
+            else:
+                self.continue_counts = 0
+                self.continue_side = Position.EMPTY
+        elif self.position == Position.LONG and (px_tuple['px'] < self.emas[-1][-1]):
+            self.position = Position.EMPTY
+            self.continue_side = Position.EMPTY
+            self.continue_counts = 0
+            if px_tuple['px'] > self.open_px:
+                self.win += 1
+            else:
+                self.loss += 1
+            return self.sell_signal()
+        elif self.position == Position.SHORT and (px_tuple['px'] > self.emas[-1][-1]):
+            self.position = Position.EMPTY
+            self.continue_side = Position.EMPTY
+            self.continue_counts = 0
+            if px_tuple['px'] < self.open_px:
+                self.win += 1
+            else:
+                self.loss += 1
+            return self.buy_signal()
+
+        return []
+        
