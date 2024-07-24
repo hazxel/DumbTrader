e432f513ea1dde770fed1148b95c029f7fe62952
hazxel <steven.b.zhou@foxmail.com>
2024-07-24 16:41:23 +0800

    semaphore wrapper finished (with basic wait/signal)

--- cpp/src/dumbtrader/ipc/posix_wrapper.h
+++ cpp/src/dumbtrader/ipc/posix_wrapper.h
@@ -4,30 +4,35 @@
 #include <semaphore.h>
 
 #include <stdexcept>
-#include <cerrno>    // includes errno
-#include <cstring>   // includes strerror
-#include <fcntl.h>   // includes O_CREAT, O_EXCL, ...
+#include <cerrno>       // errno macro (`int * __error(void)`)
+#include <cstring>      // strerror function (errno to errmsg)
+#include <fcntl.h>      // file operation flags (O_CREAT, O_EXCL, ...)
+#include <sys/stat.h>   // symbolic definitions for the permissions bits (S_IRUSR, S_IWUSR, ...)
 #include <string>
 #include <iostream>
 
 namespace dumbtrader{
+
+const unsigned int INITIAL_SEM_VALUE = 0;
+const mode_t SEM_PERM_MODE = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
 class POSIXNamedSemaphore {
 public:
     POSIXNamedSemaphore(const std::string semName) {
         semName_ = new char[semName.size() + 1]; // +1 for '\0'
         std::strcpy(semName_, semName.c_str());
-        sem_ = sem_open(semName_, O_CREAT, 0644, 1);
+        sem_ = sem_open(semName_, O_CREAT, SEM_PERM_MODE, INITIAL_SEM_VALUE);
         if (sem_ == SEM_FAILED) {
             throw std::runtime_error("Failed to initialize semaphore, errno: " + std::to_string(errno) + " (" + std::strerror(errno) + ")");
         }
     }
 
     ~POSIXNamedSemaphore() {
-        if (sem_destroy(sem_) != 0) {
-            std::cerr << "Failed to destroy semaphore, errno: " << std::to_string(errno) << " (" << std::strerror(errno) << ")" << std::endl;
+        if (sem_close(sem_) != 0) {
+            std::cerr << "Failed to close discriptor for semaphore \"" << semName_ << "\", errno: " << std::to_string(errno) << " (" << std::strerror(errno) << ")" << std::endl;
         }
         if (sem_unlink(semName_) != 0) {
-            std::cerr << "Failed to unlink semaphore, errno: " << std::to_string(errno) << " (" << std::strerror(errno) << ")" << std::endl;
+            std::cerr << "Failed to unlink semaphore \"" << semName_ << "\", errno: " << std::to_string(errno) << " (" << std::strerror(errno) << ")" << std::endl;
         }
         delete[] semName_;
     }
@@ -43,6 +48,13 @@ public:
     bool tryWait() {
         return sem_trywait(sem_) == 0;
     }
+
+    // void timedWait(int milliSec) {
+    //     struct timespec ts;
+    //     if (sem_timedwait(sem_, &ts) != 0) {
+    //         throw std::runtime_error("Failed to wait on semaphore with timeout: " + std::to_string(milliSec) + "ms");
+    //     }
+    // }
 
     void signal() {
         if (sem_post(sem_) != 0) {
