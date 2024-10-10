83e7f228bba80a48e6d52e170dd302f259618b85
hazxel <steven.b.zhou@foxmail.com>
2024-10-10 10:56:25 +0800

    ssl send with iouring

--- cpp/include/dumbtrader/utils/iouring.h
+++ cpp/include/dumbtrader/utils/iouring.h
@@ -29,7 +29,6 @@ public:
         ::io_uring_prep_read(sqe, fd, dst, len, 0);
         ::io_uring_submit(&ring_);
 
-        std::cout << "Before wait." << std::endl;
         io_uring_cqe* cqe;
         ::io_uring_wait_cqe(&ring_, &cqe);
         if (cqe->res < 0) {
@@ -39,27 +38,27 @@ public:
         
         int read = cqe->res;
         ::io_uring_cqe_seen(&ring_, cqe);
-        std::cout << "Seen sent." << std::endl;
         return read;
     }
 
     inline int write(int fd, const void* src, int len) {
-        io_uring_sqe *sqe = ::io_uring_get_sqe(&ring_);  // 从提交队列中获取一个条目
-        io_uring_prep_send(sqe, fd, src, len, 0);    // 准备发送请求
-        io_uring_submit(&ring_);  // 提交
+        io_uring_sqe *sqe = ::io_uring_get_sqe(&ring_);
+        if (!sqe) {
+            std::cerr << "Failed to get sqe" << std::endl;
+            return -1;
+        }
 
-        // 等待发送完成
+        io_uring_prep_send(sqe, fd, src, len, 0);
+        io_uring_submit(&ring_);
+
         struct io_uring_cqe *cqe;
         io_uring_wait_cqe(&ring_, &cqe);
-
-        // 检查发送是否成功
         if (cqe->res < 0) {
             fprintf(stderr, "send failed: %s\n", strerror(-cqe->res));
         } else {
             printf("Message sent successfully!\n");
         }
 
-        // 完成后标记 CQE 为已处理
         int sent = cqe->res;
         io_uring_cqe_seen(&ring_, cqe);
         return sent;
@@ -67,7 +66,6 @@ public:
 
 private:
     io_uring ring_;
-    // char buffer_[BUFFER_SIZE];
 };
 
 } // namespace dumbtrader::utils::iouring
--- cpp/src/dumbtrader/network/openssl.cpp
+++ cpp/src/dumbtrader/network/openssl.cpp
@@ -219,8 +219,8 @@ int SSLIoUringClient::write(const void *src, size_t len) {
             logOpenSSLError(err);
             throw getException(err);
         }
-        // uring_.write(socket_.getFd(), buffer_, readBioBytes);
-        socket_.send(buffer_, readBioBytes);
+        uring_.write(socket_.getFd(), buffer_, readBioBytes);
+        // socket_.send(buffer_, readBioBytes);
     }
     return totalWritten;
 }
